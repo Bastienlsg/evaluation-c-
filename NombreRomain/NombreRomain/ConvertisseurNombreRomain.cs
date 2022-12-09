2d15b071bd371dfe8cd222720bab0174391599ee
Bastienlsg <95766791+Bastienlsg@users.noreply.github.com>
2022-12-09 10:09:34 +0100

    refactoring de ConvertisseurNombreRomain

--- NombreRomain/NombreRomain/ConvertisseurNombreRomain.cs
+++ NombreRomain/NombreRomain/ConvertisseurNombreRomain.cs
@@ -7,12 +7,8 @@ namespace NombreRomain
         public static string Convertir(int nombre)
         {
             switch(nombre) {
-                case 1:
-                    return "I";
-                case 2:
-                    return "II";
-                case 3:
-                    return "III";
+                case <=3:
+                    return new string('I', nombre);
                 default:
                     throw new NotImplementedException();
             }
