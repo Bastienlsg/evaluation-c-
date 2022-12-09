5caa7e6994aaef9f256f077d474e44566f4d27dc
Bastienlsg <95766791+Bastienlsg@users.noreply.github.com>
2022-12-09 10:07:54 +0100

    ajout du nombre 3

--- NombreRomain/NombreRomain.Test/NombreRomain.cs
+++ NombreRomain/NombreRomain.Test/NombreRomain.cs
@@ -5,9 +5,11 @@ namespace NombreRomain.Test
         [Theory]
         [InlineData(1)]
         [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
         public void TestUnités(int n)
         {
-            // ETANT DONNE un nombre <n> compris entre 1 et 2
+            // ETANT DONNE un nombre <n> compris entre 1 et 3
             // QUAND on le convertit en nombres romains
             var resultat = ConvertisseurNombreRomain.Convertir(n);
 
--- NombreRomain/NombreRomain/ConvertisseurNombreRomain.cs
+++ NombreRomain/NombreRomain/ConvertisseurNombreRomain.cs
@@ -11,6 +11,8 @@ namespace NombreRomain
                     return "I";
                 case 2:
                     return "II";
+                case 3:
+                    return "III";
                 default:
                     throw new NotImplementedException();
             }
